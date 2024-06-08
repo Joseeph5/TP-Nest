7dffec2bd6a3fe4ca36c26d8e1e3b6f84dfd897f
Youssef Benamor <youssef.beenamor@gmail.com>
2024-06-08 10:13:18 +0100

    add swagger

---
+++ src/room/dto/create-room.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateRoomDto {
+  @ApiProperty({ description: 'The name of the room' })
+  name: string;
+
+  @ApiProperty({ description: 'The description of the room', required: false })
+  roomType?: string;
+
+  @ApiProperty({ description: 'The capacity of the room' })
+  roomPrice: number;
+}
--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -11,30 +11,61 @@ import {
 import { RoomService } from './room.service';
 import { Room } from './schemas/room.schema';
 
+import { CreateRoomDto } from './dto/create-room.dto';
+import { UpdateRoomDto } from './dto/update-room.dto';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+
+@ApiTags('rooms')
 @Controller('rooms')
 export class RoomController {
   constructor(private readonly roomsService: RoomService) {}
 
+  @ApiOperation({ summary: 'Get all rooms' })
+  @ApiResponse({ status: 200, description: 'Return all rooms.' })
   @Get()
   findAll(): Promise<Room[]> {
     return this.roomsService.findAll();
   }
 
+  @ApiOperation({ summary: 'Get a room by ID' })
+  @ApiResponse({ status: 200, description: 'Return the room.' })
+  @ApiResponse({ status: 404, description: 'Room not found.' })
   @Get(':id')
   findOne(@Param('id') id: string): Promise<Room> {
     return this.roomsService.findOne(id);
   }
 
+  @ApiOperation({ summary: 'Create a new room' })
+  @ApiResponse({
+    status: 201,
+    description: 'The room has been successfully created.',
+  })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
   @Post('add-room')
-  create(@Body() room: Room): Promise<Room> {
-    return this.roomsService.create(room);
+  create(@Body() createRoomDto: CreateRoomDto): Promise<Room> {
+    return this.roomsService.create(createRoomDto as Room);
   }
 
+  @ApiOperation({ summary: 'Update a room by ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'The room has been successfully updated.',
+  })
+  @ApiResponse({ status: 404, description: 'Room not found.' })
   @Put('update/:id')
-  update(@Param('id') id: string, @Body() room: Room): Promise<Room> {
-    return this.roomsService.update(id, room);
+  update(
+    @Param('id') id: string,
+    @Body() updateRoomDto: UpdateRoomDto,
+  ): Promise<Room> {
+    return this.roomsService.update(id, updateRoomDto as Room);
   }
 
+  @ApiOperation({ summary: 'Delete a room by ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'The room has been successfully deleted.',
+  })
+  @ApiResponse({ status: 404, description: 'Room not found.' })
   @Delete(':id')
   remove(@Param('id') id: string): Promise<void> {
     return this.roomsService.remove(id);
